b9a7c2b5988a44be819084a34cfdf63be8bf5fbe
Weatherlights <hauke@hauke.us>
2024-03-14 11:14:58 +0100

    New signatures, changed log file name

---
+++ Code/Modules/ComplianceReport.ps1
@@ -0,0 +1,33 @@
+﻿
+$ReportObj = @{};
+
+
+function Set-Overallcompliance {
+    param (
+        [string]$OverallCompliance
+    )
+
+    Set-ReportSetting -name "OverallCompliance" -value $OverallCompliance
+}
+
+
+function Set-ReportSetting {
+    param(
+        [string]$name,
+        [string]$value
+    )
+
+    $ReportObj.$Name = $value;
+}
+
+
+function Set-IniReportSetting {
+    param(
+        [string]$section,
+        [string]$key,
+        [string]$value
+    )
+
+
+
+}
